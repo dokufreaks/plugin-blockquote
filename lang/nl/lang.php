b57fd57af9ca1be2c12a9058e9be2a62e69d6134
Myeongjin <aranet100@gmail.com>
2013-09-25 02:20:24 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -4,5 +4,6 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author mprins <mprins@users.sf.net>
+ * @author Myeongjin <aranet100@gmail.com>
  */
-$lang['qb_blockquote']         = 'Markeer tekstblok als citaat';
+$lang['qb_blockquote']         = '텍스트를 따옴표로 표시';
